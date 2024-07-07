81832a474a6b28b9b124563448f9e7d9265ef4cb
hinamei <hina.gntm@gmail.com>
2024-07-07 11:54:23 +0900

    first commit

---
+++ realtime-recognition.js
@@ -0,0 +1,15 @@
+reearth.ui.show(`
+    <style>
+      body {
+        margin: 0;
+      }
+    </style>
+    <body>
+        <button type="button" onclick="displayDailyData()">データ取得</button>
+    </body>
+    <script>
+    function displayDailyData(){
+        alert("hoge")
+    };
+    </script>
+  `);
